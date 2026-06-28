2c81ce7c0a8ef105241394d336166c075ca0d1ad
Anthony Liu <anthonyliu5261@gmail.com>
2026-06-27 22:45:45 -0700

    add ACU and CHARGER can + remove Orbis

--- include/CANInterface.h
+++ include/CANInterface.h
@@ -9,7 +9,7 @@
 
 /**
  * @brief CAN setup function for flexcan devices
- * 
+ *
  * @tparam CAN_DEVICE the type of the CAN device being used (flexcan has all sorts of template args im not gonna call out here)
  * @param CAN_dev ref to the CAN device to be setup
  * @param baudrate the baudrate to set the device to have
@@ -34,27 +34,30 @@ void handle_CAN_setup(CAN_DEVICE& CAN_dev, uint32_t baudrate, void (*on_recv_fun
 */
 
 /**
- * @enum CanInterfaceType_e  
- * 
- * Provides the different can interfaces we use 
- * so that they can be referenced in generic methods 
+ * @enum CanInterfaceType_e
+ *
+ * Provides the different can interfaces we use
+ * so that they can be referenced in generic methods
  * such as process_ring_buffer
  */
-enum CANInterfaceType_e {
-    TELEM, 
-    RAUX, 
-    INVERTER,
-    FAUX
+enum CANInterfaceType_e
+{
+    ACU,
+    CHARGER,
+    FAUX,
+    RAUX,
+    TELEM,
+    INVERTER
 };
 
 /**
  * @brief handles reading from a receive buffer updating the current message frame from a specific receive buffer. pass through messages to the callback specified using the delegate function
- * 
+ *
  * @tparam BufferType CAN message receive buffer type (::pop_front(buf, len))
  * @tparam InterfaceContainer the type of struct holding refs / pointers to CAN interfaces that have receive callbacks
  * @param rx_buffer ref to receive buffer being received from
  * @param interfaces ref to interfaces struct (passed to the recv_switch_function)
- * @param curr_millis current millis 
+ * @param curr_millis current millis
  * @param recv_switch_func the receive function that gets called and is given the interfaces ref, CAN message struct and millis timestamp. expected to contain switch statement.
  */
 template <typename BufferType, typename InterfaceContainer>
@@ -90,7 +93,7 @@ namespace CAN_util
         memmove(buf, &can_msg, sizeof(CAN_message_t));
         CAN_msg_out_queue.push_back(buf, sizeof(CAN_message_t));
     }
-    
+
 }
 
 #endif /* CANINTERFACE */
